c98675ad28d3ec8dfca43ce1ba09066d0813e03d
anne <richardson.ae@gmail.com>
2015-06-23 21:21:55 -0500

    added logic for fixed image array sample

--- app/controllers/meme_controller.rb
+++ app/controllers/meme_controller.rb
@@ -1,4 +1,7 @@
 class MemeController < ApplicationController
   def goatme
-  end
-end
+    @siagallery = ["sia1.png", "sia2.png", "sia3.png", "sia4.png", "sia5.png", "sia6.png", "sia7.png", "sia8.png", "sia9.png", "sia10.png", "sia11.png", "sia12.png", "sia13.png", "sia14.png"]
+    @siapic = @siagallery.sample
+
+  end # goatme
+end # class
